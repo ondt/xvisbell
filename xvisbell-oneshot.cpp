a5c6cdb722642fe44a4e1ee2ddc90bfaa360d093
Ondrej Telka <ondrejtelka@gmail.com>
2020-08-08 16:08:18 +0200

    Fix formatting

--- xvisbell-oneshot.cpp
+++ xvisbell-oneshot.cpp
@@ -43,11 +43,13 @@ const bool use_black_color = true;
 
 // Flash overlay window size: x, y, width, height
 // -1 for width or height means same as your screen's size.
-struct { int x, y; int w, h; }
-geometry = {0, 0, -1, -1};
+struct {
+	int x, y;
+	int w, h;
+} geometry = {0, 0, -1, -1};
 
 // Flash duration              { seconds, microseconds }  (1 sec = 1 million µs)
-const struct timeval window_timeout = {0, 16667*4};
+const struct timeval window_timeout = {0, 16667 * 4};
 
 // Theoretically this could be set to the duration of a single frame to minimize
 //   interruption, and a single black frame might be noticeable even at 144 Hz
@@ -66,138 +68,138 @@ const struct timeval window_timeout = {0, 16667*4};
 
 
 
-bool operator<(const struct timeval & a,
-               const struct timeval & b) {
-  return timercmp(&a, &b, <);
+bool operator<(const struct timeval &a,
+               const struct timeval &b) {
+	return timercmp(&a, &b, <);
 }
 
-struct timeval & operator+=(struct timeval & a, const struct timeval & b) {
-  timeradd(&a, &b, &a);
-  return a;
+struct timeval &operator+=(struct timeval &a, const struct timeval &b) {
+	timeradd(&a, &b, &a);
+	return a;
 }
 
-struct timeval operator-(const struct timeval & a,
-                         const struct timeval & b) {
-  struct timeval toret;
-  timersub(&a, &b, &toret);
-  return toret;
+struct timeval operator-(const struct timeval &a,
+                         const struct timeval &b) {
+	struct timeval toret;
+	timersub(&a, &b, &toret);
+	return toret;
 }
 
 int main() {
-  auto dpy = XOpenDisplay(nullptr);
-  if (!dpy) {
-    throw std::runtime_error("XOpenDisplay() error");
-  }
-
-  auto scr = XDefaultScreen(dpy);
-  auto root = XRootWindow(dpy, scr);
-  auto vis = XDefaultVisual(dpy, scr);
-
-  auto major = XkbMajorVersion;
-  auto minor = XkbMinorVersion;
-
-  if (!XkbLibraryVersion(&major, &minor)) {
-    throw std::runtime_error("XkbLibraryVersion() error");
-  }
-
-  major = XkbMajorVersion;
-  minor = XkbMinorVersion;
-
-  int xkb_event_base;
-  if (!XkbQueryExtension(dpy, nullptr, &xkb_event_base,
-                         nullptr, &major, &minor)) {
-    throw std::runtime_error("XkbQueryExtension() error");
-  }
-
-  XkbSelectEvents(dpy, XkbUseCoreKbd, XkbBellNotifyMask, XkbBellNotifyMask);
-
-  unsigned int auto_ctrls, auto_values;
-  auto_ctrls = auto_values = XkbAudibleBellMask;
-
-  XkbSetAutoResetControls(dpy, XkbAudibleBellMask, &auto_ctrls, &auto_values);
-  XkbChangeEnabledControls(dpy, XkbUseCoreKbd, XkbAudibleBellMask, 0);
-
-  XSetWindowAttributes attrs;
-  if (use_black_color)
-    attrs.background_pixel = BlackPixel(dpy, scr);
-  else
-    attrs.background_pixel = WhitePixel(dpy, scr);
-  attrs.override_redirect = True;
-  attrs.save_under = True;
-
-  auto x11_fd = ConnectionNumber(dpy);
-
-  struct timeval future_wakeup;
-  bool timeout_is_set = false;
-
-  auto width = geometry.w < 0 ? DisplayWidth(dpy, scr) : geometry.w;
-  auto height = geometry.h < 0 ? DisplayHeight(dpy, scr) : geometry.h;
-
-  auto win = XCreateWindow(dpy, root, geometry.x, geometry.y,
-                           width, height, 0,
-                           XDefaultDepth(dpy, scr), InputOutput,
-                           vis,
-                           CWBackPixel | CWOverrideRedirect | CWSaveUnder,
-                           &attrs);
-
-  // from the question at https://stackoverflow.com/q/21780789
-  XserverRegion region = XFixesCreateRegion(dpy, NULL, 0);
-  XFixesSetWindowShapeRegion(dpy, win, ShapeBounding, 0, 0, 0);
-  XFixesSetWindowShapeRegion(dpy, win, ShapeInput, 0, 0, region);
-  XFixesDestroyRegion(dpy, region);
-
-  while (true) {
-    struct timeval tv, *wait_tv = nullptr;
-
-    fd_set in_fds;
-    FD_ZERO(&in_fds);
-    FD_SET(x11_fd, &in_fds);
-
-    if (timeout_is_set) {
-      // we really should use a monotonic clock here but
-      // there isn't really a portable interface for that yet
-      // in the future we can ifdef this for mac/linux
-      struct timeval cur_time;
-      if (gettimeofday(&cur_time, nullptr) < 0) {
-        throw std::runtime_error("getttimeofday() error!");
-      }
-
-      // c++ magic, in your face linus!
-      tv = (future_wakeup < cur_time
-            ? timeval{0, 0}
-            : future_wakeup - cur_time);
-      wait_tv = &tv;
-    }
-
-    while (true) {
-      if (select(x11_fd + 1, &in_fds, nullptr, nullptr, wait_tv) < 0) {
-        if (errno == EINTR) continue;
-        throw std::runtime_error("select() error!");
-      }
-      break;
-    }
-
-    if (timeout_is_set) {
-      struct timeval cur_time;
-      if (gettimeofday(&cur_time, nullptr) < 0) {
-        throw std::runtime_error("getttimeofday() error!");
-      }
-
-      if (future_wakeup < cur_time) {
-        // timeout fired
-        XUnmapWindow(dpy, win);
-        timeout_is_set = false;
-        exit(0);
-      }
-    }
-
-      XMapRaised(dpy, win);
-
-      // reset timeout
-      timeout_is_set = true;
-      if (gettimeofday(&future_wakeup, nullptr) < 0) {
-        throw std::runtime_error("getttimeofday() error!");
-      }
-      future_wakeup += window_timeout;
-  }
+	auto dpy = XOpenDisplay(nullptr);
+	if (!dpy) {
+		throw std::runtime_error("XOpenDisplay() error");
+	}
+	
+	auto scr = XDefaultScreen(dpy);
+	auto root = XRootWindow(dpy, scr);
+	auto vis = XDefaultVisual(dpy, scr);
+	
+	auto major = XkbMajorVersion;
+	auto minor = XkbMinorVersion;
+	
+	if (!XkbLibraryVersion(&major, &minor)) {
+		throw std::runtime_error("XkbLibraryVersion() error");
+	}
+	
+	major = XkbMajorVersion;
+	minor = XkbMinorVersion;
+	
+	int xkb_event_base;
+	if (!XkbQueryExtension(dpy, nullptr, &xkb_event_base,
+	                       nullptr, &major, &minor)) {
+		throw std::runtime_error("XkbQueryExtension() error");
+	}
+	
+	XkbSelectEvents(dpy, XkbUseCoreKbd, XkbBellNotifyMask, XkbBellNotifyMask);
+	
+	unsigned int auto_ctrls, auto_values;
+	auto_ctrls = auto_values = XkbAudibleBellMask;
+	
+	XkbSetAutoResetControls(dpy, XkbAudibleBellMask, &auto_ctrls, &auto_values);
+	XkbChangeEnabledControls(dpy, XkbUseCoreKbd, XkbAudibleBellMask, 0);
+	
+	XSetWindowAttributes attrs;
+	if (use_black_color)
+		attrs.background_pixel = BlackPixel(dpy, scr);
+	else
+		attrs.background_pixel = WhitePixel(dpy, scr);
+	attrs.override_redirect = True;
+	attrs.save_under = True;
+	
+	auto x11_fd = ConnectionNumber(dpy);
+	
+	struct timeval future_wakeup;
+	bool timeout_is_set = false;
+	
+	auto width = geometry.w < 0 ? DisplayWidth(dpy, scr) : geometry.w;
+	auto height = geometry.h < 0 ? DisplayHeight(dpy, scr) : geometry.h;
+	
+	auto win = XCreateWindow(dpy, root, geometry.x, geometry.y,
+	                         width, height, 0,
+	                         XDefaultDepth(dpy, scr), InputOutput,
+	                         vis,
+	                         CWBackPixel | CWOverrideRedirect | CWSaveUnder,
+	                         &attrs);
+	
+	// from the question at https://stackoverflow.com/q/21780789
+	XserverRegion region = XFixesCreateRegion(dpy, NULL, 0);
+	XFixesSetWindowShapeRegion(dpy, win, ShapeBounding, 0, 0, 0);
+	XFixesSetWindowShapeRegion(dpy, win, ShapeInput, 0, 0, region);
+	XFixesDestroyRegion(dpy, region);
+	
+	while (true) {
+		struct timeval tv, *wait_tv = nullptr;
+		
+		fd_set in_fds;
+		FD_ZERO(&in_fds);
+		FD_SET(x11_fd, &in_fds);
+		
+		if (timeout_is_set) {
+			// we really should use a monotonic clock here but
+			// there isn't really a portable interface for that yet
+			// in the future we can ifdef this for mac/linux
+			struct timeval cur_time;
+			if (gettimeofday(&cur_time, nullptr) < 0) {
+				throw std::runtime_error("getttimeofday() error!");
+			}
+			
+			// c++ magic, in your face linus!
+			tv = (future_wakeup < cur_time
+			      ? timeval{0, 0}
+			      : future_wakeup - cur_time);
+			wait_tv = &tv;
+		}
+		
+		while (true) {
+			if (select(x11_fd + 1, &in_fds, nullptr, nullptr, wait_tv) < 0) {
+				if (errno == EINTR) continue;
+				throw std::runtime_error("select() error!");
+			}
+			break;
+		}
+		
+		if (timeout_is_set) {
+			struct timeval cur_time;
+			if (gettimeofday(&cur_time, nullptr) < 0) {
+				throw std::runtime_error("getttimeofday() error!");
+			}
+			
+			if (future_wakeup < cur_time) {
+				// timeout fired
+				XUnmapWindow(dpy, win);
+				timeout_is_set = false;
+				exit(0);
+			}
+		}
+		
+		XMapRaised(dpy, win);
+		
+		// reset timeout
+		timeout_is_set = true;
+		if (gettimeofday(&future_wakeup, nullptr) < 0) {
+			throw std::runtime_error("getttimeofday() error!");
+		}
+		future_wakeup += window_timeout;
+	}
 }
